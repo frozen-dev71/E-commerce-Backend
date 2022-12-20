15e4de51cdc631bdb1b1191696b09c7f36bcb720
miyu-tanaka7 <michaeljohnshane@Gmail.com>
2022-12-20 21:57:14 +0300

    Enabled Swagger for the project

--- Ecommerce/src/main/java/com/ecommerce/Ecommerce/EcommerceApplication.java
+++ Ecommerce/src/main/java/com/ecommerce/Ecommerce/EcommerceApplication.java
@@ -2,8 +2,10 @@ package com.ecommerce.Ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class EcommerceApplication {
 
 	public static void main(String[] args) {
